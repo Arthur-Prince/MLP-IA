5205cc369d98935ed81a5371d7b6e290447cbfdc
Arthur-Prince <56482309+Arthur-Prince@users.noreply.github.com>
2021-06-19 19:38:11 -0300

    Delete MLP directory

--- MLP/src/Funcao.java
+++
@@ -1,9 +0,0 @@
-
-
-public interface Funcao {
-	public double funcao(double x);
-	
-	public double derivadaFuncao(double x);
-	
-	public double funcaoDeErro(double[]corretor , Camada atual , int k);
-}
