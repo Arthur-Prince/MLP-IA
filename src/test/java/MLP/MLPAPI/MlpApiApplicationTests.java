6a6b94092802487364444427b558777d67455a2c
Arthur-Prince <56482309+Arthur-Prince@users.noreply.github.com>
2021-06-19 19:50:25 -0300

    Add files via upload

---
+++ src/test/java/MLP/MLPAPI/MlpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package MLP.MLPAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
